5ba305735959cd282ca7470f4e043e4439a0a462
Koduri, Rohan (CGI Federal) <rohan.koduri@cgifederal.com>
2020-08-13 16:34:13 -0400

    header bar + stylings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="App-header">
+        <div style={{justifyContent:"left", marginLeft:"10px"}}>
+          <h1 >Rohan Koduri</h1>
+        </div>
+        <div style={{justifyContent: "flex-end"}}>
+        <a href="#">Contact Me</a>
+          <a href="#">About Me</a>
+          <a href="#">My Experience</a>
+          <a  href="#">Home</a>
+        </div>
+      </div>
+      <div className="App-body">
+        
+
+      </div>
     </div>
   );
 }
