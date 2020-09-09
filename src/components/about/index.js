f054e48edec5d5bb2af31fdb53cfc65e0d8862c2
Koduri, Rohan (CGI Federal) <rohan.koduri@cgifederal.com>
2020-09-09 11:38:55 -0400

    add uncommitted changes

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ScrollArrow from '../../components/ScrollArrow'
 import Accordion from '../../components/accordion'
-
+import countries from './countries.json'
 
 
 function About() {
     return(
         <div>  
-        <div><ScrollArrow/> </div> 
+        <div style={{marginLeft:"350px"}}><ScrollArrow/> </div> 
            <div>
             <iframe title="myFrame" src="https://www.google.com/maps/d/embed?mid=1VaFPnBPsY4J398dDAXG_BKtWJvjwKEb_" width="640" height="480"></iframe>
            </div>
            <div>
-           <Accordion
-             title="Argentina"
-             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            />
+           
+            {countries.map((item) => (
+                <Accordion 
+                    title = {item.title}
+                    content = {item.content}
+                />
+            ))}
            </div>
         </div>
     );   
